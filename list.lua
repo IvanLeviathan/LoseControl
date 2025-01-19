9bb7468416df79143d545c87ef2f47162302dd34
IvanLeviathan <dante100368@gmail.com>
2025-01-19 17:15:01 +0300

    v0.4

--- list.lua
+++ list.lua
@@ -4,6 +4,13 @@ local ccList = {
     'Freeze Up', "Alliance Soldier's Pull", 'Freeze of Evil Spirit', 'Freeze',
     'Bubble Trap', 'Wave Meteor Strike Hit', "Red Dragon's Wind Gust",
     "Hell Spear", 'Petrified', 'Dazed', 'Splashdown Bubble',
-    'Strong Telekinesis', 'Telekinesis', 'Silence'
+    'Strong Telekinesis', 'Telekinesis', 'Silence', "Alkaran's Curse",
+    "Meteor Impact", 'Pulled', 'Disarmed', 'Horrorshow', 'Petrified Taunt',
+    "Karim's Shackle", 'Shockstep Effect', "Agrax's Snare", 'Steel Trap',
+    'Hammer Smash', 'Ice Wall', 'Earthen Grip', "Phantasm's Wail",
+    "Ecktom's Shackle", 'Ice Shard', 'Bestial Leap', 'Explosive Ice',
+    'Dizziness', 'Unconscious', 'Ice Shock', "Alexander's Powerful Freeze",
+    "Trap!", 'Ice', 'Lava Trap Explosion', 'Howl of Terror', 'Eternal Silence',
+    'Mana Force', 'Shock', 'Poor Landing', 'Paralyzed', 'Wraith Rooted'
 }
 return ccList
